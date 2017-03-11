f3a757a541b8510cbabaa8f295176c68ce7f0bce
yc chen <gruenen@gmail.com>
2017-03-12 00:31:03 +0800

    fav and search btn

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
 var dataItems = data;
-d3.select('#board-list')
-    .selectAll('div')
-    .data(dataItems)
-    .enter()
-    .append('div')
-    .classed('board-item', true)
-    .append('img')
-    .attr('src', function(d) {return d.item_photo;})
-    .style('width', '100%');
+var card = d3.select('#board-list')
+            .selectAll('div')
+            .data(dataItems)
+            .enter()
+            .append('div')
+            .classed('board-item', true);
+
+    card.append('img')
+        .attr('src', function(d) {return d.item_photo;})
+        .classed('card-image', true);
+
+    card.append('div')
+        .classed('bottom-right', true)
+        .append('div')
+        .classed('card-hover-text', true)
+        .text(function(d) { return d.item_name +'/\n '+ d.item_city;});
+
+    card.append('div')
+        .classed('top-right', true)
+        .append('div')
+        .classed('glyphicon', true)
+        .classed('glyphicon-heart', true);
+
+    card.append('div')
+        .classed('top-right-2', true)
+        .append('div')
+        .classed('glyphicon', true)
+        .classed('glyphicon-search', true)
+        .on('click', function() {
+            console.log('clicked');
+        });
