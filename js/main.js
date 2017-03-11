de0f33427127b59d33d5fc09630b8f239cdf6c8f
Summit Suen <summit.suen@gmail.com>
2017-03-12 00:52:38 +0800

    [DEV] Added scroll functionality.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var dataItems = data;
+
 d3.select('#board-list')
     .selectAll('div')
     .data(dataItems)
@@ -8,3 +9,19 @@ d3.select('#board-list')
     .append('img')
     .attr('src', function(d) {return d.item_photo;})
     .style('width', '100%');
+
+var SCROLL_UNIT = 240;
+var scrollStop = null;
+
+$('.scroll_left').on('click', function() {
+	var target = $('div:first-child', $(this).parent());
+	var current = $(target).scrollLeft();
+	var step = SCROLL_UNIT;
+	$(target).scrollLeft(current - step);
+});
+$('.scroll_right').on('click', function() {
+	var target = $('div:first-child', $(this).parent());
+	var current = $(target).scrollLeft();
+	var step = SCROLL_UNIT;
+	$(target).scrollLeft(current + step);
+});
